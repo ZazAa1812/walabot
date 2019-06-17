bab629a798959d0e480680006946353e3d7c065a
Zharif Zubaidi <zaztech96@gmail.com>
2019-06-17 12:59:00 +1000

    Tried to depth calculation but not accurate

--- scripts/processBscan.py
+++ scripts/processBscan.py
@@ -3,17 +3,33 @@ import rospy
 from walabot.msg import rawSignal
 import matplotlib.pyplot as plt
 import numpy as np
-from walabot.msg import processData
+import time
+import math
+# speed of light m/s
+c = 299792458
+dielectric_constant = 1
 
 def callback(data):
+    global c, dielectric_constant
     amplitude = data.amplitude
     timeAxis = data.time  
-    rospy.loginfo(amplitude) 
+    # rospy.loginfo(amplitude) 
+    strongAmplitude = max(amplitude)
+    print(strongAmplitude)
+    indexAmp = amplitude.index(strongAmplitude) 
+    tcurrent = timeAxis[indexAmp]
+    tinitial = timeAxis[0]
+    # travel_speed_pulse, v
+    v = (c/math.sqrt(dielectric_constant))*10**-9
+    print(v)
+    print(tcurrent - tinitial)
+    depth = (tcurrent - tinitial)/(2*v)*1000
+    print(depth)
 
 
 def processAscan():
-    rospy.init_node('processAscan', anonymous=True)
-    rospy.Subscriber("processedAbsAscanData", processData, callback)
+    rospy.init_node('processBscan', anonymous=True)
+    rospy.Subscriber("rawSignal", rawSignal, callback)
     rospy.spin()
         
 if __name__ == '__main__':
