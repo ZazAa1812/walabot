74aee2362c4c874939885dc5d7b816666fe6debe
Zharif Zubaidi <zaztech96@gmail.com>
2019-06-18 06:26:44 +1000

    Debug message for sync

--- scripts/old_Scripts/livePlottingTest.py
+++ scripts/old_Scripts/livePlottingTest.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 import rospy
 from walabot.msg import signal
-from std_msgs.msg import Int32
-import matplotlib.pyplot as plt
 import numpy as np
-import message_filters
 
+amp = []
+dist = 0
 
-def callback(rawSignal, distance):
-    x = rawSignal.amplitude
+def callback(data):
+    amp = data.amplitude
+    x = livePlot()
+    print x
 
 def livePlot():
-    rospy.init_node('processBscan', anonymous=True)
-    rawSignal = rospy.Subscriber("rawSignal", signal)
-    distance = rospy.Subscriber("distanceTurtleTravelled", Int32)
-    ts = message_filters.TimeSynchronizer([rawSignal, distance],10)
-    ts.registerCallback(callback)
-    rospy.spin()
+    global dist
+    dist = dist + 1
+    return dist
         
 if __name__ == '__main__':
-    livePlot()
+    rospy.init_node('processBscan', anonymous=True)
+    rawSignal = rospy.Subscriber("rawSignal", signal, callback)
+    rospy.spin()
