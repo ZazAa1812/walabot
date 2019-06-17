4ba94440f073436aca7f660fd9afea20cee46546
Zharif Zubaidi <zaztech96@gmail.com>
2019-06-18 05:13:27 +1000

    Finish mock distance travelled by Turtlebot

--- scripts/distanceTurtlebotTravelled.py
+++ scripts/distanceTurtlebotTravelled.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
-
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Int32
 def distanceTrav():
-    pub = rospy.Publisher('distanceTbotTravelled', String, queue_size=1000)
+    pub = rospy.Publisher('distanceTbotTravelled', Int32, queue_size=1000)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(1) # 1hz
+    hello_str = 1
     while not rospy.is_shutdown():
-        hello_str = "%s" % rospy.get_time()
+        
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
-
+        hello_str = hello_str+1
 if __name__ == '__main__':
     try:
         distanceTrav()
