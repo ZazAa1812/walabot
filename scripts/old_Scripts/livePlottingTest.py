3e35deb7421e49f58398e92c1b804d0164245656
Zharif Zubaidi <zaztech96@gmail.com>
2019-06-18 09:37:21 +1000

    Success pcolor implementation
    Just for one scan reading

--- scripts/old_Scripts/livePlottingTest.py
+++ scripts/old_Scripts/livePlottingTest.py
@@ -7,33 +7,34 @@ import matplotlib.pyplot as plt
 dist = 0
 x = []
 y = []
-zsa = []
+z = []
 
 def callback(data):
-    global line1,x,y,zsa
-
+    global line1,x,y,z
+  
     raw = np.asarray(data.amplitude)
     temp = abs(raw)
     amp = temp.tolist()
     tempDist = xdistance()
     x.append(tempDist)
     y.append(amp)
+    z.append(amp)
+    z = np.asarray(z)
+    print(len(y))
+    print(len(y[0]))
     a = np.random.rand(1,6)
-    print a
-    # zsa.append(a)
+
     # Plotting
     plt.ion()
     plt.clf()
     plt.plot()
-    plt.pcolor(a)
+    plt.pcolor(z)
     plt.title('default')
     plt.tight_layout()
+    z = z.tolist()
     plt.pause(0.5)
     plt.show()
-
-    print zsa
-    print x
-    print len(y)
+    
 
 def xdistance():
     global dist
