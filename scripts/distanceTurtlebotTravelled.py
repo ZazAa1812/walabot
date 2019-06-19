9f42db2512edb78501a2057fdb6441276acdb633
Zharif Zubaidi <zaztech96@gmail.com>
2019-06-19 12:35:52 +1000

    Working on subscribing to odometry

--- scripts/distanceTurtlebotTravelled.py
+++ scripts/distanceTurtlebotTravelled.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import rospy
-from walabot.msg import distanceTravelled
 from geometry_msgs.msg import Twist
 import math
 import time
@@ -12,7 +11,7 @@ def move(speed, distance, is_forward):
     global temp
     #declare a Twist message to send velocity commands
     velocity_message = Twist()
-    distance_message = distanceTravelled()
+    # distance_message = distanceTravelled()
     #get current location 
     if (speed > 0.4):
         print 'speed must be lower than 0.4'
@@ -27,7 +26,7 @@ def move(speed, distance, is_forward):
     loop_rate = rospy.Rate(10) # we publish the velocity at 10 Hz (10 times a second)    
     cmd_vel_topic='/cmd_vel'
     velocity_publisher = rospy.Publisher(cmd_vel_topic, Twist, queue_size=10)
-    distance_publisher = rospy.Publisher('distanceTravelled',distanceTravelled,queue_size=10)
+    # distance_publisher = rospy.Publisher('distanceTravelled',distanceTravelled,queue_size=10)
     t0 = rospy.Time.now().to_sec()
 
     while not rospy.is_shutdown() :
@@ -37,11 +36,11 @@ def move(speed, distance, is_forward):
         t1 =  rospy.Time.now().to_sec()
         # Calculating distance travelled
         distance_moved = (t1-t0) * speed
-        distance_moved = round(distance_moved,2)
-        temp.append(distance_moved)
-        distance_message.distance = temp
-        distance_publisher.publish(distance_message)
-        print  temp
+        # distance_moved = round(distance_moved,2)
+        # temp.append(distance_moved)
+        # distance_message.distance = temp
+        # distance_publisher.publish(distance_message)
+        # print(type(temp))
         # rospy.loginfo(distance_message)          
         if  not (distance_moved<distance):
             rospy.loginfo("reached")
--- scripts/processBscan.py
+++ scripts/processBscan.py
@@ -3,7 +3,7 @@ import rospy
 from walabot.msg import signal
 import numpy as np
 import matplotlib.pyplot as plt
-from walabot.msg import distanceTravelled
+from nav_msgs.msg import Odometry
 import message_filters
 import math
 
@@ -13,49 +13,48 @@ dist = []
 i = 0
 
 def callback(rawSignaldata, distanceTravelleddata):
-# def callback(data):
     global y,z,dist,i
     # convert amplitude value into array for computing
     raw = rawSignaldata.amplitude
-    distraw = distanceTravelleddata.distance
+    distraw = distanceTravelleddata.pose
     rawAmp = raw[0:2000]
     # updating list for plotting
     z.append(rawAmp)
     dist = list(distraw)
-    print(z)
+    print(dist)
     print(type(dist))
     # inverting amplitude value to get a vertical plot
-    z = zip(*z)
-    y_min, y_max = np.asarray(z).min(), np.asarray(z).max()
-    # Plotting raw signal
-    if i==0:
-        plt.figure(figsize=(15,8))
-        i = i + 1
-    plt.clf()
-    plt.ylim(2000,0)
-    plt.xlim(0,50)
-    plt.pcolormesh(z,cmap = 'gist_gray',vmin=y_min, vmax=y_max)
-    plt.title('Bscan')
-    plt.ylabel('Number of sample data')
-    plt.xlabel('Mock distance Travelled')
-    plt.xticks(np.arange(len(dist)),dist,rotation = 45)
-    plt.draw()
-    plt.pause(0.0000001)
-    plt.show()
-    z = zip(*z)
-    print (len(z))
-    if len(z)==50:
-        shutcommand = "shutdown"
-        plt.savefig('WalabotBscan.pdf',bbox_inches='tight')
-        rospy.signal_shutdown(shutcommand)
-    # clear for new list
+    # z = zip(*z)
+    # y_min, y_max = np.asarray(z).min(), np.asarray(z).max()
+    # # Plotting raw signal
+    # if i==0:
+    #     plt.figure(figsize=(15,8))
+    #     i = i + 1
+    # plt.clf()
+    # plt.ylim(2000,0)
+    # plt.xlim(0,50)
+    # plt.pcolormesh(z,cmap = 'gist_gray',vmin=y_min, vmax=y_max)
+    # plt.title('Bscan')
+    # plt.ylabel('Number of sample data')
+    # plt.xlabel('Mock distance Travelled')
+    # plt.xticks(np.arange(len(dist)),dist,rotation = 45)
+    # plt.draw()
+    # plt.pause(0.0000001)
+    # plt.show()
+    # z = zip(*z)
+    # print (len(z))
+    # if len(z)==50:
+    #     shutcommand = "shutdown"
+    #     plt.savefig('WalabotBscan.pdf',bbox_inches='tight')
+    #     rospy.signal_shutdown(shutcommand)
+    # # clear for new list
     dist = []
         
 if __name__ == '__main__':
     rospy.init_node('processBscan', anonymous=True)
     plt.ion()
     rawSignaldata = message_filters.Subscriber("rawSignal", signal)
-    distanceTurtleTravdata = message_filters.Subscriber('distanceTravelled',distanceTravelled)
+    distanceTurtleTravdata = message_filters.Subscriber('odom',Odometry)
     ts = message_filters.ApproximateTimeSynchronizer([rawSignaldata, distanceTurtleTravdata],10,0.1)
     ts.registerCallback(callback)
     rospy.spin()
