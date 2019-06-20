e44fd71ed78dd9182fb4d40532f35fc2c6a21fe4
Zharif Zubaidi <zaztech96@gmail.com>
2019-06-20 13:03:34 +1000

    Bscan run

--- scripts/distanceTurtlebotTravelled.py
+++ scripts/distanceTurtlebotTravelled.py
@@ -3,6 +3,8 @@ import rospy
 from geometry_msgs.msg import Twist
 import math
 import time
+import xlwt
+from xlwt import Workbook
 
 temp = []
 
@@ -28,22 +30,32 @@ def move(speed, distance, is_forward):
     velocity_publisher = rospy.Publisher(cmd_vel_topic, Twist, queue_size=10)
     # distance_publisher = rospy.Publisher('distanceTravelled',distanceTravelled,queue_size=10)
     t0 = rospy.Time.now().to_sec()
-
-    while not rospy.is_shutdown() :
+ ################ Write data of max amplitude for each scan for depth calibration #################
+    wb = Workbook()
+    sheet1 = wb.add_sheet('Sheet 1')
+    sheet1.write(0,0, 'Timestamp')
+    sheet1.write(0,1, 'Distance')
+    i = 1
+    while not rospy.is_shutdown():
         rospy.loginfo("Turtlesim moves forwards")
         velocity_publisher.publish(velocity_message)
         loop_rate.sleep()
         t1 =  rospy.Time.now().to_sec()
         # Calculating distance travelled
         distance_moved = (t1-t0) * speed
-        # distance_moved = round(distance_moved,2)
-        # temp.append(distance_moved)
+        distance_moved = round(distance_moved,2)
+        temp.append(distance_moved)
         # distance_message.distance = temp
         # distance_publisher.publish(distance_message)
         # print(type(temp))
-        # rospy.loginfo(distance_message)          
+        rospy.loginfo(t1)
+        rospy.loginfo(distance_moved)
+        sheet1.write(i,0,t1)
+        sheet1.write(i,1,distance_moved) 
+        i = i+1         
         if  not (distance_moved<distance):
             rospy.loginfo("reached")
+            wb.save('src/walabot/data/dataBscan/distanceTimestamp.xls')
             break
     #finally, stop the robot when the distance is moved
     velocity_message.linear.x =0
@@ -52,6 +64,7 @@ def move(speed, distance, is_forward):
 if __name__ == '__main__':
     try:
         rospy.init_node('turtle_motion',anonymous=True)
-        move(0.05,1.0,True)
+        rospy.sleep(5)
+        move(0.1,0.8,True)
     except rospy.ROSInterruptException or KeyboardInterrupt:
         pass
--- scripts/graphBscan.py
+++ scripts/graphBscan.py
@@ -5,47 +5,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 from walabot.msg import distanceTravelled
 import message_filters
+import xlwt
+from xlwt import Workbook
 
 y = []
 z = []
 dist = []
 i = 0 # Counter for mock distance value
-
+j=0
 def callback(data):
-    global y,z,dist,i
+    global y,z,dist,i,j
     # convert amplitude value into array for computing
     raw = data.amplitude
     rawAmp = raw[0:2000]
     # updating list for plotting
     z.append(rawAmp)
     dist.append(i)
-    print(dist)
+    # print(dist)
     # inverting amplitude value to get a vertical plot
     z = zip(*z)
     y_min, y_max = np.asarray(z).min(), np.asarray(z).max()
     # Plotting raw signal
     if i==0:
         plt.figure(figsize=(15,8))
+        wb = Workbook()
+        sheet1 = wb.add_sheet('Sheet 1')
+        sheet1.write(0,0, 'Timestamp')
+        sheet1.write(0,1, 'Distance')
     plt.clf()
     plt.ylim(2000,0)
-    plt.xlim(0,50)
+    plt.xlim(0,40)
     plt.pcolormesh(z,cmap = 'gist_gray',vmin=y_min, vmax=y_max)
     plt.title('Bscan')
     plt.ylabel('Number of sample data')
     plt.xlabel('Mock distance Travelled')
     plt.xticks(np.arange(len(dist)),dist,rotation = 45)
-    plt.draw()
+    plt.draw() 
     plt.pause(0.0000001)
     plt.show()
     z = zip(*z)
     print (len(z))
+    tc = rospy.Time.now().to_sec()
+    print tc
+    # sheet1.write(j,0,tc)
+    # sheet1.write(j,1,i)
+    # j = j +1
     i = i + 0.02
-    if len(z)==50:
+    if len(z)==40:
         i = "shutdown"
         plt.savefig('WalabotBscan.png',bbox_inches='tight')
         rospy.signal_shutdown(i)
 if __name__ == '__main__':
     rospy.init_node('processBscan', anonymous=True)
     plt.ion()
+    rospy.sleep(1)
     rospy.Subscriber("rawSignal",signal,callback)
     rospy.spin()
